2f19f87b1091084d52cd4ff65ab3cdea70f9406e
Tacolinge <128391785+Tacolinge@users.noreply.github.com>
2023-06-17 20:24:17 +0200

    WiP med API

--- API_uib.py
+++ API_uib.py
@@ -1,13 +1,10 @@
-import requests
+#import requests
 import time
+import json
+import os
 
-def make_article_num(number):
-    counter = 0
-    while counter < number:
-        counter += 1
-        #print(counter)
-        get_article_url(counter)
-
+#find_path_to_json_file = os.getcwd() + "\test_lemma_til_api.json"
+relative_path = "En-setning-et-ord-Data-behandling\test_lemma_til_api.json"
 def get_article_url(article_id):
     api_url = "https://ord.uib.no/bm/article/"
     file_json = ".json"
@@ -17,10 +14,23 @@ def get_article_url(article_id):
 #test = requests.get("https://ord.uib.no/bm/article/1.json")
 
 
+def get_article_number(json_file):
+    f = open(json_file)
+    data = json.load(f)
+    for i in data:          #struktur på listen er ["ordet", artikkelnr, "ordklasse"]
+        get_article_number(i[1])
+    
+    f.close
+
+
+
+
+
 if __name__ == "__main__":
     start_timer = time.time()
-    make_article_num(100)
-    get_article_url(1)
+    os.getcwd()
+
+#    get_article_number(relative_path)
 
     end_timer = time.time()
     print(end_timer - start_timer)
