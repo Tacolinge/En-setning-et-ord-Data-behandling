2b31b7d0ba29c429081fd4252669890f9af8ae9f
Tacolinge <128391785+Tacolinge@users.noreply.github.com>
2023-06-26 13:53:38 +0200

    removed dups form json file

---
+++ Clean_explanations.py
@@ -0,0 +1,27 @@
+import json
+import collect_values as cv
+file = r'C:\Users\fortn\Desktop\Programmerings prosjekter\En Setning Et Ord\data-behandling\En-setning-et-ord-Data-behandling\cleanWordDataBm.json'
+
+
+# for å luke ut ord uten fullstndig forklaring
+def clean_explanations(file):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        keep_data = []
+        counter = 0
+        ord = []
+        for e in data:
+            if isinstance(e["explanation"], dict):
+                ny_verdi = cv.make_missing_word_list(e["explanation"])
+                if not isinstance(ny_verdi, str):
+                    counter += 1
+                    ord.append(e["Word"])
+                    continue
+                e["explanation"] = ny_verdi
+            keep_data.append(e)
+    with open("cleanWordDataBm.json", "w", encoding="utf-8") as new_file:
+        json.dump(keep_data, new_file, indent=4,
+                  ensure_ascii=False, sort_keys=True)
+
+    f.close
+    print(ord, counter, len(keep_data), sep='\n')
---
+++ remove_dups_from_json.py
@@ -0,0 +1,31 @@
+import json
+
+file = r'C:\Users\fortn\Desktop\Programmerings prosjekter\En Setning Et Ord\data-behandling\En-setning-et-ord-Data-behandling\cleanWordDataBm.json'
+
+# fjerner også singel bokstav ord som "ø"
+
+
+def remove_dups_from_json(file):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        new_data = []
+        unique_words = set()
+
+        for item in data:
+            word = item.get("Word")
+            if len(word) <= 1:
+                print(word)
+            if word not in unique_words:
+                unique_words.add(word)
+                new_data.append(item)
+            else:
+                print("removed dupe:", word)
+
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(new_data, f, indent=4, ensure_ascii=False)
+        print("Removed words complete unique words count: ", len(unique_words))
+        f.close
+    f.close
+
+
+remove_dups_from_json(file)
